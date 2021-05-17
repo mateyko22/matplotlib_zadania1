970f4f1f1e9604445c99258f8af3d9a846c20b73
mateyko22 <petmat@o2.pl>
2021-05-17 11:57:24 +0200

    zad

--- main.py
+++ main.py
@@ -3,3 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn
 
+#Zad 1
+# x = np.arange(20, 41)
+# print(x)
+#
+# plt.plot(x, 1/x, label="f(x) = 1/x")
+#
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.legend()
+# plt.title("Wykres f(x) = 1/x")
+# plt.axis([20, max(x), 0.02, 0.05])
+# plt.show()
+
+#Zad 2
+# plt.plot(x, 1/x, 'bo--', label='f(x) = 1/x')
+# plt.xlabel('x')
+# plt.ylabel('1/x')
+# plt.title("Wykres funkcji f(x) x[20, 40]")
+# plt.show()
+
+#Zad 3
+# x = np.arange(0, 45, 0.1)
+# plt.plot(x, np.cos(x), label='f(x) = cos(x)')
+# plt.plot(x, np.sin(x), label='f(x) = sin(x)')
+# plt.legend()
+# plt.xlabel('x')
+# plt.ylabel('sin(x)\ncos(x)')
+# plt.title('Wykres sin(x) oraz cos(x)')
+# plt.show()
+
+#Zad 4
